0c3bb05f046c2f9fa415b98917b09466fc9ba771
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-07-14 20:22:16 +0900

    사용자 계정 추천 리스트 수정
    머하웃 추천 시스템을 활용하여 사용자 입력 키워드 기준으로 유사도를 측정해 사용자 추천 계정 리스트를 만듦

---
+++ planit/src/main/java/com/planit/domain/main/RecommendDTO.java
@@ -0,0 +1,11 @@
+package com.planit.domain.main;
+
+import lombok.Data;
+
+@Data
+public class RecommendDTO {
+    private int userNo;
+    private int plantKeyId;
+    private int prefer;
+    private String userId;
+}
